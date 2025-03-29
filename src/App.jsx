7d039517e60c08c100b2bbf6f1141f5e3bd6ff9d
Karen <navarreteruizkarensofia@gmail.com>
2025-03-29 12:03:59 -0600

    Implementacion de daisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+   const [count, setCount] = useState(0)
 
-  return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-blue-500">¡Hola, Soy LinkUp editado por karen omg! 🚀</h1>
-    </div>
-  )
+   return (
+      <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+         <h1 className="text-4xl font-bold text-blue-500">¡Hola, Soy LinkUp editado por karen omg! 🚀</h1>
+         <button className="btn">Hola</button>
+      </div>
+   )
 }
 
 export default App
